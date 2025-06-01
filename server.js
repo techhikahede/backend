7e1fc67bc9222894d991575eafd9ff35ea1408ac
techhikahede <techtalk0629@gmail.com>
2025-06-01 18:25:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors"
 
 import customerRoutes from "./routes/customer.route.js";
 import orderRoutes from "./routes/order.route.js";
 import campaignRoutes from "./routes/campaign.route.js";
 import campaignHistoryRoutes from "./routes/campaignHistory.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+import { protect } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
@@ -15,14 +19,23 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
+
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.use("/api/v1/customers", customerRoutes);
-app.use("/api/v1/orders", orderRoutes);
-app.use("/api/v1/campaigns", campaignRoutes);
-app.use("/api/v1/campaignHistory", campaignHistoryRoutes);
+// Auth route - no protection needed
+app.use("/api/v1/auth", authRoutes);
+
+// Protect your resource routes with the middleware
+app.use("/api/v1/customers", protect, customerRoutes);
+app.use("/api/v1/orders", protect, orderRoutes);
+app.use("/api/v1/campaigns", protect, campaignRoutes);
+app.use("/api/v1/campaignHistory", protect, campaignHistoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
