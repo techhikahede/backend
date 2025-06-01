d7b7c55f2af72a9fe13e200231482ee09b3a9a34
Riyasatt <kriyasatali04@gmail.com>
2025-06-01 15:52:27 +0530

    Gemini integreted for generating segment rules

--- routes/campaign.route.js
+++ routes/campaign.route.js
@@ -6,12 +6,14 @@ import {
   getCampaignById,
   updateCampaign,
   deleteCampaign,
+  parseNaturalLanguageByGemini,
 } from "../controllers/campaign.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createCampaign);
 router.post("/preview", previewCampaign);
+router.post("/aiModel", parseNaturalLanguageByGemini)
 router.get("/", getAllCampaigns);
 router.get("/:id", getCampaignById);
 router.put("/:id", updateCampaign);
