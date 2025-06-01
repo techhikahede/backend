5fa25cd47ad0e917dd70253c79a319ab21b060c8
techhikahede <techtalk0629@gmail.com>
2025-06-01 18:24:18 +0530

    Delete routes directory

--- routes/order.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createOrder, getAllOrders } from "../controllers/order.controller.js";
-
-const router = express.Router();
-
-router.post("/create", createOrder);
-router.get("/all", getAllOrders);
-
-export default router;
